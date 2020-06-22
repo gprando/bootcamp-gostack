087daa4f65f5cb826e4fdb0e118b3a0760f5d6d7
Gabriel Prando <gprando55@gmail.com>
2020-06-22 10:49:44 -0300

    clean

--- typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { hellow } from './routes';
-
-const app = express();
-
-app.get('/', hellow);
-
-app.listen(3333);
--- typescript/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-import createUser from './services/CreateUser';
-
-export function hellow(req: Request, res: Response) {
-  const user = createUser({ email: 'gabriel', password: '1234', techs: ['node', 'react'] });
-
-  return res.json({ message: 'hellow world' });
-}
