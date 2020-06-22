01c074bc1ffb966f8cad5594db19f288bc947aac
Gabriel Prando <gprando55@gmail.com>
2020-06-22 11:32:40 -0300

    config lints

--- fase-02-primeiro-projeto-nodejs/src/server.ts
+++ fase-02-primeiro-projeto-nodejs/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'hellow world' });
-})
+app.get('/', (request, response) => response.json({ message: 'hellow world' }));
 
 app.listen(3333, () => {
-  console.log('Server running in port 3333!')
+  console.log('Server running in port 3333!');
 });
-
